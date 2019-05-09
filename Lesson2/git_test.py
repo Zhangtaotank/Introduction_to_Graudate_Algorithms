6cd45c5108e1f56af2abfb93a0911d2cf4fe6361
tank <zhangtaotank@gmail.com>
2019-05-08 21:48:28 -0400

    Add some change to test GitHub

--- Lesson2/git_test.py
+++ Lesson2/git_test.py
@@ -3,6 +3,6 @@ colors = ['red', 'yellow', 'green', 'blue']
 i = 0
 while i < len(colors):
     
-    print('When i was %d, my favoriate color is %s' % (i, colors[i]))
+    print('When i was %d, my favorite color is %s' % (i, colors[i]))
     i = i + 1
 
